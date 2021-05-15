197449274c3ee0dc038f86bd5b9d8ac8b1b0b2b4
Chirag0401 <chip2741@gmail.com>
2021-05-15 20:16:57 +0530

    Prediction Page Changes

--- vaayu/so2_predictor.py
+++ vaayu/so2_predictor.py
@@ -64,19 +64,6 @@ class so2_predictor:
         
         return prediction.ravel()[0]
     
-    def get_wide_deep_prediction(self, data):
-        X_deep = self.__get_dataframe(data, 'wide deep deep')
-        X_wide = self.__get_dataframe(data, 'wide deep wide')
-        deep_scaler = joblib.load('saved_scalers/so2/so2_wide_deep_deep_scaler.save')
-        wide_scaler = joblib.load('saved_scalers/so2/so2_wide_deep_wide_scaler.save')
-        
-        X_deep = deep_scaler.transform(X_deep)
-        X_wide = wide_scaler.transform(X_wide)
-        
-        model = keras.models.load_model('saved_models/so2/so2_wide_deep.h5')
-        prediction = model.predict((X_deep, X_wide))
-        
-        return prediction.ravel()[0]
     
     def get_lstm_prediction(self, data):
         X = self.__get_dataframe(data, 'lstm')
@@ -89,16 +76,6 @@ class so2_predictor:
         
         return prediction.ravel()[0]
     
-    def get_gru_prediction(self, data):
-        X = self.__get_dataframe(data, 'gru')
-        scaler = joblib.load('saved_scalers/so2/so2_gru_scaler.save')
-        X = scaler.transform(X)
-        model = keras.models.load_model('saved_models/so2/so2_GRU.h5')
-        X = np.reshape(X, (X.shape[0], 1, X.shape[1]))
-        
-        prediction = model.predict(X)
-        
-        return prediction.ravel()[0]
 
 
 # import sys, os
